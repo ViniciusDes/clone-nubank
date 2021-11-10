70182fe9ec8dccd7aa8b77e4d834d8a218ba84e6
viniciusdes <viniciussilva9200@gmail.com>
2021-11-10 01:35:03 -0300

    inicio projeto

---
+++ src/pages/Main/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '~/Components/Header';
+
+import {Container} from './styles';
+
+export default function Main() {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+}
